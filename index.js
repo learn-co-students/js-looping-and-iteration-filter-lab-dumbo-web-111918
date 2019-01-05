a9bae0e859e5cced84539ab8a74c7f4f7362bfc1
John Mark Redding <johnmarkredding@gmail.com>
2019-01-05 23:14:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file
+function findMatching(drivers, string) {
+  return drivers.filter(function (elm) {
+    return (elm.toLowerCase() === string.toLowerCase())
+  });
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(function (elm) {
+    return (elm.slice(0, string.length).toLowerCase() === string.toLowerCase())
+  });
+}
+
+function matchName(drivers, string) {
+  return drivers.filter(function (elm) {
+    return (elm.name.toLowerCase() === string.toLowerCase())
+  });
+}
