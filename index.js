e5cb7406d67023e64c0bbaa3da96dd36ddbc65a2
Jesus Escalona <jesusescalona@me.com>
2019-01-05 13:38:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+matchName = (drivers, str) => (
+  drivers.filter( e => e.name === str)
+  );
+
+findMatching = (drivers, str) => (
+  drivers.filter(e => e.toLowerCase() === str.toLowerCase() )
+  );
+
+fuzzyMatch = (drivers, str) => (
+  drivers.filter(e => e.startsWith(str) )
+  );
