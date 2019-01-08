41524ed099a8c289852a59e88a02823f7ef87046
Joseph <joeephus@gmail.com>
2019-01-07 22:42:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file
+function findMatching(arr, str) {
+    str = str.toLowerCase();
+    return arr.filter(function( i ) {
+        return i.toLowerCase() === str
+    });
+}
+
+function fuzzyMatch(arr, partialStr) {
+    strLength = partialStr.length
+    return arr.filter(function(name) {
+        return name.slice(0, strLength) === partialStr
+    })
+}
+
+function matchName(arr, key) {
+    return arr.filter(function(obj){
+        return obj["name"].toLowerCase() === key.toLowerCase();
+    });
+};
